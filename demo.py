5700cec7be4539d8f04127e6908c9e74f2d33818
Alex <52550827+alex-wilson-gwf@users.noreply.github.com>
2023-06-19 14:43:07 +1000

    Update demo.py
    removed asyncio.coroutine as incompatible with Py > 3.11
    formatted text outputs

--- demo.py
+++ demo.py
@@ -7,14 +7,18 @@ from airtouch4pyapi import AirTouch, AirTouchStatus, AirTouchVersion
 
 def print_groups(groups):
     for group in groups:
-        print(f"Group Name: {group.GroupName:15s} Group Number: {group.GroupNumber:3d} PowerState: {group.PowerState:3s} IsOn: {group.IsOn} OpenPercent: {group.OpenPercent:3d} Temperature: {group.Temperature:3.1f} Target: {group.TargetSetpoint:3.1f} BelongToAc: {group.BelongsToAc:2d} Spill: {group.Spill}")
+        print(f"Group Name: {group.GroupName:15s} | Group Number: {group.GroupNumber:3d} | PowerState: {group.PowerState:3s} | IsOn: {group.IsOn:3s} | OpenPercent: {group.OpenPercent:3d} | Temperature: {group.Temperature:3.1f} | Target: {group.TargetSetpoint:3.1f} | ControlMethod: {group.ControlMethod:19s} | BelongToAc: {group.BelongsToAc:2d} | Spill: {group.Spill}")
 
+def print_attr(obj):
+    max_length = 0
+    for x in dir(obj):
+        if x[:2] != '__' and len(x) > max_length:
+            max_length = len(x)
+    for x in dir(obj):
+        if x[:2] != '__':
+            print(f"{x:{str(max_length) + 's'}} : {getattr(obj, x)}")
 
-def print_acs(acs):
-    for ac in acs:
-        print(f"AC Name: {ac.AcName:15s} AC Number: {ac.AcNumber:3d} IsOn: {ac.IsOn} PowerState: {ac.PowerState:3s} Target: {ac.AcTargetSetpoint:3.1f} Temp: {ac.Temperature:3.1f} Modes Supported: {ac.ModeSupported} Fans Supported: {ac.FanSpeedSupported} startGroup: {ac.StartGroupNumber: 2d} GroupCount: {ac.GroupCount:2d} Spill: {ac.Spill}")
-
-async def updateInfoAndDisplay(ip) -> asyncio.coroutine:
+async def updateInfoAndDisplay(ip):
     at = AirTouch(ip)
     await at.UpdateInfo()
     if(at.Status != AirTouchStatus.OK):
@@ -24,7 +28,8 @@ async def updateInfoAndDisplay(ip) -> asyncio.coroutine:
     print("AC INFO")
     print("----------")
     acs = at.GetAcs()
-    print_acs(acs)
+    for ac in acs:
+        print_attr(ac)
     print("----------\n\n")
     print("GROUP/ZONE INFO")
     print("----------")
@@ -71,23 +76,9 @@ async def updateInfoAndDisplay(ip) -> asyncio.coroutine:
         print("AC INFO AFTER SETTING GROUP 0 to COOL")
         print("----------")
         print_acs(acs)
-#    await at.TurnGroupOff(0)
-#    print("Turned off group 0, sleeping 4")
-#    time.sleep(4);
-#    await at.TurnGroupOn(0)
-#    print("Turned on group 0")
-    
-#    await at.TurnAcOff(0)
-#    print("Turned off ac 0, sleeping 4")
-#    time.sleep(4);
-#    await at.TurnAcOn(0)
-#    print("Turned on ac 0")
-#    print(at.GetSupportedFanSpeedsByGroup(0))
-#    await at.SetGroupToPercentByGroupName("Zone 1", 5)
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print("nom nom nom give me an IP of an AirTouch system")
         sys.exit(1)
     asyncio.run(updateInfoAndDisplay(sys.argv[1]))
-    
